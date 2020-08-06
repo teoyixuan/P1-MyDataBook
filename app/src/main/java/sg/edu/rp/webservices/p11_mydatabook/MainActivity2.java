c7795eb0400f7911a2ec95a39f8c397cbc80371a
18041430 <18041430@myrp.edu.sg>
2020-08-06 13:34:19 +0800

    Done

--- app/src/main/java/sg/edu/rp/webservices/p11_mydatabook/MainActivity2.java
+++ app/src/main/java/sg/edu/rp/webservices/p11_mydatabook/MainActivity2.java
@@ -3,12 +3,22 @@ package sg.edu.rp.webservices.p11_mydatabook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        iv = findViewById(R.id.iv);
+
+        String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
+        Picasso.with(this).load(imageUrl).into(iv);
     }
 }
